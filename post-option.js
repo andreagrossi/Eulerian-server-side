d89d2875bdf41ecc252df7c77fb54a0020d35997
andreagrossi <34943851+andreagrossi@users.noreply.github.com>
2023-10-05 10:57:44 +0200

    Update post-option.js

--- post-option.js
+++ post-option.js
@@ -5,6 +5,14 @@ const userParameters = {
   etagid: 'etagid_for this tag', // Id of the tag is declared in the Tag Management interface for debugging purposes
 };
 
+// Define the replay parameters
+const replayParameters = {
+  'ereplay-time': 'epoch_time_of_the_hit'1599210524, // Epoch time at which the hit has been recorded.
+  'ereplay-ip': 'user_ip_address', // IP address of the client that generated the hit. If not provided this will have an impact on the geographic resolution.
+  'ereplay-ua': 'uuser_agent', // User-Agent of the client that generated the hit. If not provided this will have an impact on device resolution and device filtering.
+  'ereplay-mdevicetype': 'Desktop', // Device type of the client (e.g., Desktop, Phone, Tablet, etc.)
+};
+
 // Define the parameters you want to pass in the POST body
 const postParameters = {
   parameter1: 'value1',
@@ -15,15 +23,16 @@ const postParameters = {
 // Add the CACHE_BUSTER value as a GET parameter (ea-rnd parameter)
 const cacheBusterValue = 'CACHE_BUSTER_VALUE'; // Replace with your actual cache buster value
 
-// URL of the destination endpoint
+// URL of the destination endpoint with cache buster as a query parameter
 //tracking hostname : your tracking sub domain
 //type : collector
 //website name : your website name
 const url = `https://TRACKING_HOSTNAME/TYPE/WEBSITE_NAME/?ea-rnd=${cacheBusterValue}`;
 
-// Combine user identification and POST parameters
+// Combine user identification, replay, and POST parameters
 const combinedParameters = {
   ...userParameters,
+  ...replayParameters,
   ...postParameters,
 };
 
